4d0309e9aff37d055fbe5ef1333e8ae7fe610e0c
Imtiyaz <imtiyaz7602@gmail.com>
2024-10-15 03:34:37 +0530

    second

--- mcqs_pointer.cpp
+++ mcqs_pointer.cpp
@@ -2,7 +2,44 @@
 using namespace std;
 
 int main(){
-    
-
+    // int first=8;
+    // int second=18;
+    // int *ptr=&second;
+    // *ptr=9;
+    // cout<<first<<" "<<second<<endl;
+    // int first=6;
+    // int *p=&first;
+    // int *q=p;
+    // (*q)++;
+    // cout<<first<<endl;
+    // int first=8;
+    // int *p=&first;
+    // cout<<(*p)++<<" "<<endl;
+    // cout<<first<<endl;
+    // int *p=0;
+    // int first=110;
+    // *p=first;
+    // cout<<*p<<endl;
+    // int first=8;
+    // int second=11;
+    // int *third=&second;
+    // first=*third;
+    // *third=*third+2;
+    // cout<<first<<" "<<second<<endl;
+    // float f=12.5;
+    // float p=21.5;
+    // float *ptr=&f;
+    // (*ptr)++;
+    // *ptr=p;
+    // cout<<*ptr<<" "<<f<<" "<<p<<endl;
+    // int arr[5];
+    // int *ptr;
+    // cout<<sizeof(arr)<<" "<<sizeof(ptr)<<endl;
+    int arr[]={11,21,13,14};
+    // cout<<*(arr)<<" "<<*(arr+1)<<endl;
+    // cout<<arr<<" "<<&arr<<endl;
+    // cout<<(arr+1)<<endl;
+    int *p=arr;
+    cout<<p[2]<<endl;
     return 0;
 }
